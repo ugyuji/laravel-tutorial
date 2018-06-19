461b5530decb9cd5579ffe7c4aa041fed315bb90
Yuji Tsunomori <y.tsunomori@flag-pictures.co.jp>
2018-06-19 14:31:55 +0900

    Finish pages

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'LSAPP') }}</title>
-    </head>
-    <body>
-        <h1>INDEX</h1>
-        <p>This is INDEX</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron">
+      <h1 class="display-4">{{ $title }}</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+      <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+    </div>
+@endsection
